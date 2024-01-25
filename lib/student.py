755579ee0625324676d8638e5485807880d6fda3
Boniface <cheruiyotbony@gmail.com>
2024-01-25 10:24:32 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,18 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge=[]
+    
+    def learn(self,n):
         pass
+        return self.knowledge.append(n)
+    
+    
+    
+    
+    
+Boniface =Student('Boniface','cheruiyot')
+Boniface.learn('Physics')
+print(Boniface.knowledge)
